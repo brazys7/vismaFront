27ab95169e174f1c45dfadba53cd23d85e880b9c
brazys7 <48604517+brazys7@users.noreply.github.com>
2020-12-27 18:58:03 +0200

    showing items, deleting item from the list

--- app.js
+++ app.js
@@ -1,21 +1,125 @@
 class Event {
-    constructor(description, deadline) {
+    constructor(description, deadline, completedAt) {
         this.description = description;
         this.deadline = deadline;
-        this.completedAt = null;
-    }
+        this.completedAt = completedAt;        
+    }    
 
     completeEvent() {
-        this.completedAt = new Date();
+        if(this.completedAt === null) {
+            this.completedAt = new Date();
+        }
+        else {
+            this.completedAt = null;
+        }        
     }
 }
 
-let events = [];
+let storedEvents = [];
 
 prepareDOM();
 
 function prepareDOM() {
     todayDate();
+
+    if(readFromStorage()) {
+        let template = ``;
+        let index = 0;
+
+        storedEvents.forEach(item => {
+            
+            template += `
+            <div class="event">
+                <div class="doneEvent">
+                    <input class="toggleDone" type="checkbox" onclick="complateEventByIndex(` + index + `)"`; 
+            
+            if(item.completedAt !== null) {
+                template += `checked`;
+            }
+
+            template += `>                                
+                </div>
+                <div class="eventContent">
+                    <p class="eventTitle"`;
+            if(item.completedAt) {
+                template += `style="text-decoration: line-through;"`;
+            }
+            template += `>` + item.description + `</p>
+                    <p class="eventDeadline"><b>Time left:</b> ` + getTimeLeft(item) + `</p>
+                </div>
+                <div id="deleteItem" class="inputIcon" onclick="deleteEvent(`+ index + `)")></div>          
+            </div>
+            `;
+            index++;
+        });
+        
+        document.getElementById("eventsTable").innerHTML = template;
+    }
+    else {
+        document.getElementById("eventsHeader").innerHTML = "No upcoming events";
+        document.getElementById("tableMenu").style.display = "none";
+    }    
+}
+
+function getTimeLeft(event) {
+    if(event.completedAt) {
+        return "Done";
+    }
+    else if(!event.deadline) {
+        return "No deadline";
+    }
+    else {
+        const now = new Date();
+        const deadline = new Date(event.deadline);
+        
+        var difference = (now - new Date(event.deadline)) / 1000;
+
+        days = Math.trunc(Math.abs(difference / (60 * 60 * 24)));
+        if(days > 0) {
+            difference += days * (60 * 60 * 24);
+        }
+        hours = Math.trunc(Math.abs(difference / (60 * 60)));
+        if(hours > 0) {
+            difference += hours * (60 * 60);
+        }
+        minutes = Math.trunc(Math.abs(difference / (60)));
+        
+        let result = "";
+        let hoursValue = false;
+        let minutesValue = false;
+        if(days > 0) {
+            result += days + "D";
+            hoursValue = true;
+        }
+        if(hours > 0) {
+            if(hoursValue) {
+                result += ", "
+            }
+            result += hours + "H"
+            minutesValue = true;
+        }
+        if(minutes > 0) {
+            if(minutesValue) {
+                result += ", "
+            }
+            result += minutes + "Min"
+        }
+        return result;
+    }
+}
+
+function readFromStorage() {
+    const events = JSON.parse(sessionStorage.getItem("events"));
+    storedEvents = [];
+
+    if(events) {
+        events.forEach(event => {
+            const newEvent = new Event(event.description, event.deadline, event.completedAt);
+            storedEvents.push(newEvent);
+        })        
+        return true;
+    }
+    else return false;
 }
 //prints today date in a selected format to the DOM
 function todayDate() {
@@ -51,9 +155,25 @@ function saveItem() {
     }
     else {
        const newEvent = new Event(description, deadline);
-        events.push(newEvent);
+        storedEvents.push(newEvent);
 
         resetForm();
-        sessionStorage.setItem("events", JSON.stringify(events));   
+        saveToLocalStorage();   
+        prepareDOM();
     }     
+}
+function saveToLocalStorage() {
+    sessionStorage.setItem("events", JSON.stringify(storedEvents));
+}
+
+function complateEventByIndex(index) {
+    storedEvents[index].completeEvent();
+    
+    saveToLocalStorage();
+}
+function deleteEvent(index){
+    storedEvents.splice(index, 1);
+
+        saveToLocalStorage();
+        prepareDOM();
 }
